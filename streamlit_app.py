3fff5a350b6452b22e94f5edc0a4d77cbe50c6bf
JWoodrff <jw6938@princeton.edu>
2024-06-02 22:19:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 """
 # strands editor
 """
+circle1 = plt.Circle(( 0.5, 0.5 ), 0.2 )
 
+st.button("Reset", type="primary")
+if st.button("o"):
+    st.write(circle1)
+else:
+    st.write("no")
